afc08a104f43a15a651420e867c927e8dfdc9f4f
fareed <ucdon1249@gmail.com>
2021-05-30 02:53:51 +0500

    flash messages added

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const session = require('express-session');
+
 
 //student model
 const Student = require('./models/student');
@@ -56,6 +58,25 @@ function checkFileType(file, cb) {
     cb('ERROR : Images only !');
   }
 }
+////
+
+//express session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+//express messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+
+
+
 
 
 //////
@@ -73,7 +94,10 @@ app.post('/students/create',upload,(req, res) => {
 
    const student = new Student({name:req.body.name,batch:req.body.batch,image:req.file.filename})
  student.save()
- .then(results =>{res.redirect('/students');})
+ .then(results =>{
+   req.flash('success','Student added');
+   res.redirect('/students');
+  })
  .catch(err => {console.log(err)});
 
 });
@@ -92,6 +116,7 @@ app.get('/students/delete/:id', (req, res) => {
 
 Student.deleteOne({_id:req.params.id})
 .then(result=>{
+  req.flash('danger','students removed.');
   res.redirect('/students');
 })
 .catch(err=>{console.log(err)});
